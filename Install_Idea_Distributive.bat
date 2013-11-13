e6db62b1756880ff6898dbb671d682f2c82ea63c
VladimirKorzh <vladimirkorshak@gmail.com>
2013-11-12 21:37:13 -0800

    little tiny fix

--- Install_Idea_Distributive.bat
+++ Install_Idea_Distributive.bat
@@ -18,13 +18,13 @@ IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
 :64BIT
 echo You have 64-bit Windows.
 echo Running Anaconda Installation.
-%cd%Anaconda\Anaconda-1.8.0-Windows-x86_64.exe
+%cd%\Anaconda\Anaconda-1.8.0-Windows-x86_64.exe
 GOTO ANACONDAEND
 
 :32BIT
 echo You have 32-bit windows.
 echo Running Anaconda Installation.
-%cd%Anaconda\Anaconda-1.8.0-Windows-x86.exe
+%cd%\Anaconda\Anaconda-1.8.0-Windows-x86.exe
 GOTO ANACONDAEND
 
 :ANACONDAEND 
